70ec03b51840eed230f69c31573e3348548776f3
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2021-04-27 20:33:15 -0400

    rg351v: Add support for rg351v

---
+++ rg351v/BoardConfig.mk
@@ -0,0 +1,6 @@
+PLATFORM_PATH := device/anbernic/rg351v
+TARGET_SCREEN_DENSITY 		:= 120
+# Camera
+USE_CAMERA_STUB := false
+
+TARGET_KERNEL_CONFIG := lineageos_rg351v_defconfig
---
+++ rg351v/mkimg.sh
@@ -0,0 +1,79 @@
+#/bin/bash
+
+LINEAGEVERSION=lineage-18.1
+DATE=`date +%Y%m%d`
+IMGNAME=$LINEAGEVERSION-$DATE-rg351v.img
+IMGSIZE=7
+OUTDIR=${ANDROID_PRODUCT_OUT:="../../../out/target/product/rg351v"}
+
+if [ `id -u` != 0 ]; then
+	echo "Must be root to run script!"
+	exit
+fi
+
+if [ -f $IMGNAME ]; then
+	echo "File $IMGNAME already exists!"
+else
+	echo "Creating image file $IMGNAME..."
+	dd if=/dev/zero of=$IMGNAME bs=1M count=$(echo "$IMGSIZE*1024" | bc)
+	sync
+	echo "Creating partitions..."
+	(
+	echo o
+	echo n
+	echo p
+	echo 1
+	echo
+	echo +256M
+	echo n
+	echo p
+	echo 2
+	echo
+	echo +2000M
+	echo n
+	echo p
+	echo 3
+	echo
+	echo +256M
+	echo n
+	echo p
+	echo
+	echo
+	echo t
+	echo 1
+	echo c
+	echo a
+	echo 1
+	echo w
+	) | fdisk $IMGNAME
+	sync
+	LOOPDEV=`kpartx -av $IMGNAME | awk 'NR==1{ sub(/p[0-9]$/, "", $3); print $3 }'`
+	sync
+	if [ -z "$LOOPDEV" ]; then
+		echo "Unable to find loop device!"
+		kpartx -d $IMGNAME
+		exit
+	fi
+	echo "Image mounted as $LOOPDEV"
+	sleep 5
+	mkfs.fat -F 32 /dev/mapper/${LOOPDEV}p1 -n BOOT
+	mkfs.ext4 /dev/mapper/${LOOPDEV}p2 -L system
+	mkfs.ext4 /dev/mapper/${LOOPDEV}p3 -L vendor
+	mkfs.f2fs /dev/mapper/${LOOPDEV}p4 -l userdata
+	echo "Copying system..."
+	dd if=$OUTDIR/system.img of=/dev/mapper/${LOOPDEV}p2 bs=1M
+	echo "Copying vendor..."
+	dd if=$OUTDIR/vendor.img of=/dev/mapper/${LOOPDEV}p3 bs=1M
+	echo "Copying BOOT..."
+	mkdir -p sdcard/BOOT
+	sync
+	mount /dev/mapper/${LOOPDEV}p1 sdcard/BOOT
+	sync
+	cp BOOT/* sdcard/BOOT
+	sync
+	umount /dev/mapper/${LOOPDEV}p1
+	rm -rf sdcard
+	kpartx -d $IMGNAME
+	sync
+	echo "Done, created $IMGNAME!"
+fi

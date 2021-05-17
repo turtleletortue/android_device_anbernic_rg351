49f3d9b9d43a05cf8751b3e01d6147756c493622
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2021-05-17 15:50:05 -0400

    rg351p: Fix controller mapping * Note: No device buttons are currently mapped to home button or task switcher or mouse mode switcher

--- rg351p/device.mk
+++ rg351p/device.mk
@@ -22,8 +22,8 @@
 #
 
   	    	    	
-#PRODUCT_COPY_FILES += \
-#    	$(LOCAL_PATH)/odroidgo2_joypad.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/odroidgo2_joypad.kl \
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/Vendor_1209_Product_3100.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1209_Product_3100.kl \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.primary_display_orientation=ORIENTATION_270 \
